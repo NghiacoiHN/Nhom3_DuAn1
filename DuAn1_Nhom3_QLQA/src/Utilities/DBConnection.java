d353416847cccdc964d35846792372422d85d7e6
NghiacoiHN <nghiantph23346@fpt.edu.vn>
2022-11-14 23:33:27 +0700

    11:33PM

--- DuAn1_Nhom3_QLQA/src/Utilities/DBConnection.java
+++ DuAn1_Nhom3_QLQA/src/Utilities/DBConnection.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
 public class DBConnection {
      private static final String USERNAME = "sa";
     private static final String PASSWORD = "123456789";
-    private static final String SERVER_NAME = "NGHIACOIHN\\SQLEXPRESS";
+    private static final String SERVER_NAME = "Localhost";
     private static final String PORT = "1433";
     private static final String DATABASE_NAME = "DuAn_1";
     private static final boolean USING_SSL = true;
---
+++ Insertinto.sql
@@ -0,0 +1,25 @@
+﻿--Chất liệu
+INSERT INTO ChatLieu
+                  (MaCL, TenCL, TinhTrang)
+VALUES ('CL01',N'Nỉ',1), ('CL02',N'Bông',1)
+SELECT*FROM ChatLieu
+--Size
+INSERT INTO Size
+                  (MaSize, TenSize, TinhTrang)
+VALUES ('SZ01',N'L',1), ('SZ02',N'M',1)
+SELECT*FROM Size
+--Màu sắc
+INSERT INTO MauSac
+                  (MaMS, TenMS, TinhTrang)
+VALUES ('MS01',N'Màu Hồng',1),('MS02',N'Màu Xanh',1)
+SELECT*FROM MauSac
+--Sản phẩm
+INSERT INTO SanPham
+                  (MaSP, TenSp, TinhTrang)
+VALUES ('SP01',N'Quần',1),('SP02',N'Áo',1)
+SELECT*FROM SanPham
+--Loại sản phẩm
+INSERT INTO LoaiSP
+                  (MaLSP, TenLSP, TinhTrang)
+VALUES ('LSP01',N'Áo Blaze',1), ('LSP02',N'Quần Blaze',1)
+SELECT*FROM LoaiSP
