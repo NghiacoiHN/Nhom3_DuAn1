c9b05df088886b3027771b242f7a4861f49c5280
tonyphucvn <lathanhkc@gmail.com>
2022-11-16 19:09:58 +0700

    ok

--- DuAn1_Nhom3_QLQA/src/Sevices/impl/HoaDonSevice.java
+++ DuAn1_Nhom3_QLQA/src/Sevices/impl/HoaDonSevice.java
@@ -1,19 +1,5 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-<<<<<<< HEAD
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package Sevices.impl;
-
-import Reponsitories.*;
-import DomainModels.HoaDonDM;
-<<<<<<<< HEAD:DuAn1_Nhom3_QLQA/src/Sevices/impl/HoaDonSevice.java
-import Reponsitories.HoaDonIRepon;
-import Reponsitories.impl.HoaDonRepon;
-import Sevices.HoaDonISevice;
-========
->>>>>>>> origin/Tu4n:DuAn1_Nhom3_QLQA/src/Sevices/HoaDonSevice.java
-=======
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Sevices.impl;
@@ -22,7 +8,6 @@ import DomainModels.HoaDonDM;
 import Reponsitories.HoaDonIRepon;
 import Reponsitories.impl.HoaDonRepon;
 import Sevices.HoaDonISevice;
->>>>>>> origin/Tu4n
 import ViewModels.HoaDonVM;
 import java.util.List;
 
@@ -30,19 +15,6 @@ import java.util.List;
  *
  * @author TRONG NGHIA
  */
-<<<<<<< HEAD
-public interface HoaDonSevice {
-
-    List<HoaDonVM> findAll();
-
-    HoaDonDM getOne(String ID);
-
-<<<<<<<< HEAD:DuAn1_Nhom3_QLQA/src/Sevices/impl/HoaDonSevice.java
-    @Override
-    public HoaDonVM getOne(Integer trangThai) {
-        try {
-            return hdRepon.getOne(trangThai);
-=======
 public class HoaDonSevice implements HoaDonISevice {
 
     HoaDonIRepon hdRepon = new HoaDonRepon();
@@ -51,7 +23,6 @@ public class HoaDonSevice implements HoaDonISevice {
     public List<HoaDonVM> findAll() {
         try {
             return hdRepon.findAll();
->>>>>>> origin/Tu4n
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -59,7 +30,16 @@ public class HoaDonSevice implements HoaDonISevice {
     }
 
     @Override
-<<<<<<< HEAD
+    public HoaDonVM getOne(Integer trangThai) {
+        try {
+            return hdRepon.getOne(trangThai);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
     public boolean add(HoaDonDM a) {
         try {
             return hdRepon.add(a);
@@ -67,15 +47,6 @@ public class HoaDonSevice implements HoaDonISevice {
             e.printStackTrace();
             return false;
         }
-=======
-    public HoaDonDM getOne(String ID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean add(HoaDonDM a) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
->>>>>>> origin/Tu4n
     }
 
     @Override
@@ -87,15 +58,5 @@ public class HoaDonSevice implements HoaDonISevice {
     public boolean update(HoaDonDM a, String ID) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-<<<<<<< HEAD
-========
-    boolean add(HoaDonDM a);
 
-    boolean delete(String ma);
->>>>>>>> origin/Tu4n:DuAn1_Nhom3_QLQA/src/Sevices/HoaDonSevice.java
-
-    boolean update(HoaDonDM a, String ID);
-=======
-
->>>>>>> origin/Tu4n
 }
